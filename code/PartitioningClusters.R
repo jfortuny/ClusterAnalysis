ed3ccc446ea06ba584c639330c951cc96ffcab68
jfortuny <jfortuny@jfortuny.com>
2017-09-25 12:39:05 -0400

    continued hierarchical clustering

--- code/PartitioningClusters.R
+++ code/PartitioningClusters.R
@@ -1,4 +1,6 @@
 library(factoextra)
+library(dendextend)
+library(igraph)
 
 data("USArrests")
 df <- USArrests
@@ -81,3 +83,57 @@ fviz_cluster(clara.dfScaled,
 clara.dfScaled <- clara(dfScaled, 4, samples = 50, pamLike = TRUE)
 # Visualize clusters
 fviz_cluster(clara.dfScaled)
+
+# Visualizing dendrograms
+dd <- dist(scale(USArrests), method = "euclidean")
+hc <- hclust(dd, method = "ward.D2")
+fviz_dend(hc, cex = 0.5,
+          main = "Dendrogram - War D2",
+          xlab = "Objects", ylab = "Distance", sub = "")
+# horizontally
+fviz_dend(hc, cex = 0.5, horiz = TRUE)
+# Cutting and coloring
+fviz_dend(hc, k = 4, cex = 0.5,
+          k_colors = c("#2E9FDF", "#00AFBB", "#E7B800", "#FC4E07"),
+          color_labels_by_k = TRUE,
+          rect = TRUE,
+          rect_border = c("#2E9FDF", "#00AFBB", "#E7B800", "#FC4E07"),
+          rect_fill = TRUE)
+# Color palettes: RColorBrewer or ggsci
+fviz_dend(hc, cex = 0.5, k = 4,
+          k_colors = "jco")
+fviz_dend(hc, cex = 0.5, k = 4,
+          k_colors = "jco",
+          rect = TRUE,
+          rect_border = "jco",
+          rect_fill = TRUE)
+# Circular dendrogram
+fviz_dend(hc, cex = 0.5, k = 4,
+          k_colors = "jco",
+          type = "circular")
+# Phylogenic tree
+fviz_dend(hc, cex = 0.5, k = 4,
+          k_colors = "jco",
+          type = "phylogenic",
+          repel = TRUE)
+fviz_dend(hc, cex = 0.5, k = 4,
+          k_colors = "jco",
+          type = "phylogenic",
+          repel = TRUE,
+          phylo_layout = "layout.gem")
+
+# Zooming in a dendrogram
+# 1) Create the full dendrogram
+den_plot <- fviz_dend(hc, cex = 0.5, k = 4,
+                      k_colors = "jco")
+# 2) Get the Dendrogram's data
+den_data <- attr(den_plot, "dendrogram")
+# 3) Plot the overall dendrogram
+print(den_plot)
+# 4) Choose subtree level/cut to print
+den_cuts <- cut(den_data, h = 5)
+# 5) Print a branch
+fviz_dend(den_cuts$lower[[1]], main = "Subtree 1")
+fviz_dend(den_cuts$lower[[2]], main = "Subtree 2")
+fviz_dend(den_cuts$lower[[3]], main = "Subtree 3")
+fviz_dend(den_cuts$lower[[4]], main = "Subtree 4")
